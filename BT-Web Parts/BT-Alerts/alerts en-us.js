15b6ba80e7fa156a1b688989c7b3f1eb29910bf6
SofiaBind <31539042+SofiaBind@users.noreply.github.com>
2017-09-04 12:55:54 +0100

    Add files via upload

---
+++ BT-Web Parts/BT-Alerts/alerts en-us.js
@@ -0,0 +1,217 @@
+window.BT = window.BT || {};
+window.BT.v2alertsWP  = window.BT.v2alertsWP || {};
+window.BT.v2alertsWP.lang = window.BT.v2alertsWP.lang || {};
+window.BT.v2alertsWP.lang['en-us'] = {
+
+  "regional_settings":{
+    "moment" : "en"
+  },
+
+  "webpart_toolbar": {
+    "webpart_name"		: "Alerts",
+    "new_entry"			: "Create new Alert",
+    "properties"		: "Webpart Properties",
+    "manage"			: "Manage Alerts",
+    "list"				: "Alerts List",
+    "version"			: "Version",
+    "preview_mode"		: "Preview mode"
+  },
+
+  "webpart_new_item": {
+    "modal_heading"			: "New Notification",
+    "edit_heading"			: "Edit Notification",
+
+    "content_heading"		: "Notification Content",
+
+    "title_label"			: "Title",
+    "title_placeholder"		: "e.g. Report delivery tomorrow",
+
+    "text_label"							: "Text",
+    "text_markdown_label"					: "Text",
+    "text_hyperlink_type_text"				: "Text Link",
+    "text_hyperlink_type_tiny_button"		: "Tiny button",
+    "text_hyperlink_type_small_button"		: "Small button",
+    "text_hyperlink_type_medium_button"		: "Medium button",
+    "text_hyperlink_type_large_button"		: "Large button",
+    "text_hyperlink_address"				: "Address",
+    "text_hyperlink_display_text"			: "Display text",
+    "text_hyperlink_bg_color"				: "e.g. #000000",
+    "text_hyperlink_text_color"				: "Text color",
+    "text_hyperlink_new_window"				: "Open in new window",
+    "text_hyperlink_btn_apply"				: "Ok",
+
+    "text_image_alt_placeholder"			: "Alt text",
+    "text_image_path_placeholder"			: "Image path",
+    "text_image_btn_apply"					: "Ok",
+
+    "text_video_path_placeholder"			: "Video path",
+    "text_video_btn_apply"					: "Ok",
+    "text_info"								: "We support Markdown. Check syntax rules ",
+    "text_info_here"						: "here",
+
+
+    "styles_heading"				: "Notification Styles",
+
+    "type_label"					: "Type",
+    "type_options_default"			: "Default",
+    "type_options_success"			: "Success",
+    "type_options_warning"			: "Warning",
+    "type_options_danger"			: "Danger",
+    "type_options_info"				: "Info",
+
+    "template_label"				:  "Template",
+    "template_options_default"		: "Default",
+    "template_options_notification"	: "Notification",
+
+    "layout_label"							: "Layout",
+    "layout_options_solid_color"			: "Solid Color",
+    "layout_options_basic"					: "Basic",
+    "layout_options_light"					: "Light",
+    "layout_options_lighticon"				: "Light Icon",
+    "layout_options_dark"					: "Dark",
+    "layout_options_darkicon"				: "Dark Icon",
+
+    "placement_label"						: "Placement",
+    "placement_options_vertical_label"		: "Vertical",
+    "placement_options_vertical_top"		: "Top",
+    "placement_options_vertical_bottom"		: "Bottom",
+    "placement_options_horizontal_label"	: "Horizontal",
+    "placement_options_horizontal_right"	: "Right",
+    "placement_options_horizontal_left"		: "Left",
+    "placement_options_horizontal_center"	: "Center",
+
+    "settings_heading"						: "Notification Settings",
+
+    "autohide_label"					: "Auto-hide Delay",
+    "autohide_seconds"					: "Seconds",
+
+    "dismissable_label"					: "Dismissable",
+    "dismissable_options_yes"			: "Yes",
+    "dismissable_options_no"			: "No",
+
+    "status_label"						: "Status",
+    "status_options_active"				: "Active",
+    "status_options_inactive"			: "Inactive",
+
+    "target_users_label"				: "Target Users",
+
+    "btn_cancel"						: "Cancel",
+    "btn_preview"						: "Preview",
+    "btn_save"							: "Save",
+    "btn_save_changes"					: "Save Changes",
+    "btn_save_create"					: "Save and Create Another",
+    "btn_continue_editing"				: "Continue Editing"
+  },
+
+  "webpart_properties":{
+
+    "webpart_properties_heading"		: "Web Part Properties",
+
+    "alert_options_heading"				: "Alert Options",
+    "settings_source_label"				: "Settings Source",
+    "settings_source_nothing"			: "Nothing Selected",
+    "settings_source_copy"				: "Copy",
+    "settings_source_copy_success"		: "Properties copied successfully.",
+
+    "list_settings_heading"				: "List Settings",
+
+    "list_url_label"					: "List Url",
+    "list_url_placeholder"				: "e.g. https://bindtuning.sharepoint.com/Lists/Alerts/AllItems.aspx",
+    "styles_master_label"				: "Styles from master",
+    "styles_master_checkbox"			: "Inherit",
+
+    "advanced_options_heading"			: "Advanced Options",
+    "caching_label"						: "Caching",
+    "caching_option_nocaching"			: "No Caching (Updates Every Page Load)",
+    "caching_option_page"				: "Page Caching (updates when web part is saved)",
+    "caching_option_localstorage"		: "Local Storage Caching (updates every 30 minutes)",
+    "caching_add_new_warning"			: "With this option enabled, the web part will not update until the properties are saved or you add a new item using the web part's forms.",
+    "caching_user_targetting_warning"	: "Links with user targetting will not be displayed using this option.",
+    "caching_limited_space_warning"		: "Browsers have limited space available for local storage. Other web parts or scripts resorting to LocalStorage may lose functionality when it becomes full.",
+    "language_label"					: "Language",
+    "some_texts_warning"				: "Some texts may not be updated until the page gets refreshed.",
+
+    "webpart_appearance"				: "Web Part Appearance",
+
+    "chrome_type_label"					: "Chrome Type",
+    "chrome_type_option_default"		: "Default",
+    "chrome_type_option_none"			: "None",
+    "chrome_type_option_title_border"	: "Title and Border",
+    "chrome_type_option_title"			: "Title Only",
+    "chrome_type_option_border"			: "Border Only",
+    "webpart_title_label"				: "Web Part Title",
+    "webpart_title_default_text"		: "BindTuning Calendar",
+    "webpart_url_label"					: "Web Part URL",
+    "webpart_tooltip_label"				: "Web Part Tooltip",
+    "webpart_tooltip_default_text"		: "Centrally manage alerts and notifications within your site collection, with rich alert layouts, and targeting individual users or security groups in your Active Directory.",
+    "rtl_label"							: "Right to Left",
+    "rtl_checkbox_label"				: "Active",
+
+    "webpart_messages"					: "Web Part Messages",
+
+    "webpart_errors_label"				: "Web Part errors",
+    "webpart_errors_option_edit"		: "Show In Edit Mode Only",
+    "webpart_errors_option_always"		: "Always Show",
+    "webpart_errors_option_never"		: "Never Show",
+
+
+    "btn_copy"							: "Copy Properties",
+    "btn_close"							: "Close",
+    "btn_save"							: "Save"
+  },
+
+  "webpart_list": {
+    "modal_title"			: "Alerts List",
+    "title_label"			: "Title",
+    "template_label"		: "Template",
+    "content_label"			: "Content",
+    "active_lavel"			: "Active",
+  },
+
+  "webpart_messages": {
+    "new_notification"			: "New Notification",
+    "cache_warning"				: "With this option enabled, the web part will not update until the properties are saved or you add a new item using the web par t\'s forms.",
+    "cache_limited_warning"		: "Browsers have limited space available for local storage. Other web parts or scripts resorting to LocalStorage may lose functionality when it becomes full.",
+    "saved_successfully"		: "Web part properties saved successfully",
+    "something_wrong"			: "Oops, something went wrong:",
+    "alert_success"				: "Alert successfuly created",
+    "alert_update_success"		: "Alert successfuly updated",
+    "alert_delete_success"		: "Alert was successfully deleted.",
+    "failed_delete_alert"		: "Failed to delete Alert:",
+    "failed_retrieve_data"		: "Failed to retrieve alert data:",
+    "saving"					: "Saving...",
+    "save_changes"				: "Save Changes",
+
+    "webpart_not_configured"	: "This Web Part is not fully configured.",
+    "list_not_connected"		: "Open the Web Part properties and connect it to a list.",
+
+    "couldnt_load_list"			: "It seems we couldn't load the provided list. Please make sure you typed the correct URL.",
+    "any_questions"				: "If you have any questions on how to implement the web part, please check the user guide.",
+    "provided_list_url"			: "Provided List Url:",
+    "list_empty"				: "Your list is empty. Please add data to your list.",
+    "no_entries"				: "No entries to display",
+    "url_doesnt_match"			: "The inserted URL does not match any existing video in the Video Portal",
+    "check_provider"			: "Please check if the video provider name is typed correctly in the markdown.",
+    "allowed_providers"			: "Allowed providers: Youtube, Vimeo or Video Portal",
+    "update_available"			: "BindTuning Web Parts update available.",
+    "update"					: "Update",
+    "updating"					: "Updating web parts...",
+    "failed_save_properties"	: "Failed to save properties: ",
+    "failed_get_properties"		: "Failed to get properties from webpart: ",
+     "localstorage_full"		: "Localstorage is full and the web part was not succesfully cached.",
+     "remove_sure"				: "Are you sure you want to remove this alert?",
+
+    "saving"					: "saving...",
+    "save_changes"				: "save changes",
+    "save"						: "Save",
+    "save_create"				: "Save and create another",
+    "save_adding"				: "Adding..."
+  },
+
+  "webpart_custom_form": {
+    "properties_copied"			: "Properties copied successfully",
+    "page_checked_heading" 		: "Page is checked in!",
+    "page_checked_desc" 		: "Check Out the page and save the web part for these changes to take effect.",
+    "2_alignment"				: "Please select 2 alignment values.",
+  }
+};
