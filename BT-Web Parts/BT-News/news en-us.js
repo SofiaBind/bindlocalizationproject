2e2f7848c99133f5675ee88ba52f645b532abd2b
SofiaBind <31539042+SofiaBind@users.noreply.github.com>
2017-09-04 14:20:22 +0100

    Add files via upload

---
+++ BT-Web Parts/BT-News/news en-us.js
@@ -0,0 +1,224 @@
+window.BT = window.BT || {};
+window.BT.newsWP  = window.BT.newsWP || {};
+window.BT.newsWP.lang = window.BT.newsWP.lang || {};
+window.BT.newsWP.lang['en-us'] = {
+  "regional_settings":{
+    "moment" : "en"
+  },
+  "webpart_toolbar": {
+    "webpart_name": "News",
+    "new_entry": "Create new entry",
+    "properties": "Web Part Properties",
+    "manage": "Manage News",
+      "list": "News List",
+    "list_open": "Open News Entry List",
+    "version": "Version"
+  },
+  "webpart_new_item": {
+    "modal_heading": "Add News",
+
+    "article_preview_heading": "Article Preview",
+    "title_label": "Title",
+    "title_placeholder": "Title of the news article",
+    "summary_label": "Summary",
+    "summary_placeholder": "A short summary of the article",
+    "thumbnail_url_label": "Thumbnail Url",
+    "thumbnail_url_placeholder": "e.g. https://intranet.com/images/news_thumbnail",
+
+    "article_content_heading": "Article Content",
+    "article_url_label": "Article Url",
+    "article_url_placeholder": "e.g. https://newssite.com/news_article",
+
+    "article_image":"Article Image",
+    "article_image_eg":"e.g. https://intranet.com/images/news_image",
+    "article_body":"Article Body",
+
+    "metadata_heading": "Meta Data",
+    "category_label": "Category",
+    "publication_date_label": "Publication Date",
+
+    "btn_cancel": "Cancel",
+    "btn_save": "Save",
+    "btn_save_changes": "Save Changes",
+    "btn_save_create": "Save and Create Another"
+
+  },
+
+  "webpart_properties": {
+    "news_options": "News Options",
+
+    "modal_heading": "Web Part Properties",
+
+    "list_options_heading": "List Options",
+    "news_lists_label": "News Lists",
+    "add_list_btn": "Add List",
+    "list_placeholder": "e.g. /sites/Home/SitePages/default.aspx",
+    "select_list_placeholder": "Select List",
+    "filtering_options_label": "Filtering Options:",
+    "filtering_option_nofilters": "No Filters",
+    "filtering_option_caml": "CAML Query",
+    "field_mapping_options_label": "Field Mapping Options:",
+    "all_fields_select": "All Fields",
+    "news_title_label": "News Title",
+    "new_content_label": "News Content",
+    "news_category_label": "News Category",
+    "new_content_label": "News Content",
+    "news_url_label": "News URL",
+    "news_summary_label": "News Summary",
+    "news_date_label": "News Date",
+    "news_thumbnail_label": "News Thumbnail",
+    "news_image_label": "News Image",
+    "news_map_placeholder": "No Mapping",
+    "add_list_btn": "Add List",
+
+    "item_sorting_options_heading": "Item Sorting Options",
+    "order_by_label": "Order By",
+    "order_by_option_asc": "Ascending",
+    "order_by_option_desc": "Descending",
+    "add_order_by_btn": "Add Order By",
+
+
+    "layout_option_heading": "Layout Options",
+    "layout_template_label": "Layout Template",
+    "layout_template_option_basic": "Basic",
+    "layout_template_option_basic_cards": "Basic Cards",
+    "layout_template_option_gradient": "Gradient",
+    "layout_template_option_gradient_wide": "Gradient",
+    "layout_template_option_boxed": "Boxed",
+    "layout_template_option_condensed": "Condensed",
+    "nr_columns_label": "Number of Columns",
+    "nr_columns_option1": "1 Column",
+    "nr_columns_option2": "2 Columns",
+    "nr_columns_option3": "3 Column",
+    "nr_columns_option4": "4 Columns",
+    "display_limit_label": "Display Limit",
+
+    "content_options_heading": "Content Option",
+    "click_action_label": "Click Action",
+    "click_action_option_show": "Show Article Content",
+    "click_action_option_open": "Open Article Site",
+    "open_site_label": "Open Site In",
+    "open_site_option_new_window": "New Window",
+    "open_site_option_same_window": "Same Window",
+    "open_site_option_modal": "Modal",
+
+
+    "category_settings_heading":"Category Settings",
+    "color_coding_label":"Color coding",
+    "color_coding_checkbox":"Enabled",
+    "color_coding_settings_label":"Color coding settings",
+    "color_coding_settings_placeholder":"e.g. Category1[color]; Category2[color]",
+    "custom_icons_label":"Custom Icons",
+    "custom_icons_checkbox":"Enabled",
+    "custom_icons_settings_label":"Custom icons settings",
+    "custom_icons_settings_placeholder":"e.g. Category1[icon class]; Category2[icon class]; Category2,Category3[icon class]",
+    "custom_icons_settings_tip":"To use custom category icons, check Font Awesome examples ",
+    "custom_icons_settings_tip_link":"here",
+
+    "advanced_options_heading": "Advanced Options",
+    "caching_label": "Caching",
+    "caching_option_nocaching": "No Caching (Updates Every Page Load)",
+    "caching_option_page": "Page Caching (updates when web part is saved)",
+    "caching_option_localstorage": "Local Storage Caching (updates every 30 minutes)",
+    "caching_add_new_warning":"With this option enabled, the web part will not update until the properties are saved or you add a new item using the web part's forms.",
+    "caching_user_targetting_warning": "Links with user targetting will not be displayed using this option.",
+    "caching_limited_space_warning": "Browsers have limited space available for local storage. Other web parts or scripts resorting to LocalStorage may lose functionality when it becomes full.",
+    "language_label": "Language",
+    "some_texts_warning": "Some texts may not be updated until the page gets refreshed.",
+
+
+    "webpart_appearance": "Web Part Appearance",
+
+    "chrome_type_label": "Chrome Type",
+    "chrome_type_option_default": "Default",
+    "chrome_type_option_none": "None",
+    "chrome_type_option_title_border": "Title and Border",
+    "chrome_type_option_title": "Title Only",
+    "chrome_type_option_border": "Border Only",
+    "webpart_title_label": "Web Part Title",
+    "webpart_title_default_text": "BindTuning Calendar",
+    "webpart_url_label": "Web Part URL",
+    "webpart_tooltip_label": "Web Part Tooltip",
+    "rtl_label": "Right to Left",
+    "rtl_checkbox_label": "Active",
+
+    "webpart_messages_heading": "Web Part messages",
+    "webpart_errors_label": "Web Part errors",
+    "webpart_errors_option_edit": "Show In Edit Mode Only",
+    "webpart_errors_option_always": "Always Show",
+    "webpart_errors_option_never": "Never Show",
+
+    "btn_copy": "Copy Properties",
+    "btn_cancel": "Cancel",
+    "btn_save": "Save",
+
+    "copy_success": "Properties copied successfully."
+  },
+
+  "new_list": {
+    "news_list_heading": "News List",
+
+    "load_list": "Load List",
+
+    "title": "Title",
+    "category": "Category",
+    "date": "Date",
+    "list_name": "List Name",
+    "delete_alert": "Are you sure you want to delete this Article?",
+    "delete_success": "Article successfully deleted",
+
+  },
+
+  "webpart_document_modal": {
+    "download_btn": "Download",
+    "share_btn": "Share",
+    "close_btn": "Close"
+  },
+
+  "webpart_messages": {
+    "webpart_not_configured": "This Web Part is not fully configured.",
+    "list_not_connected": "Open the Web Part properties and connect it to a list.",
+    "localstorage_full":"BT WebParts: Localstorage is full and the web part was not succesfully cached.",
+    "couldnt_load_list": "It seems we couldn't load the provided lists. Please make sure you typed the correct URLs.",
+    "any_question_wp":"If you have any questions on how to implement the web part, please check the user guide.",
+    "provided_list_urls":"Provided list Urls:",
+    "some_lists_couldnt":"Some lists couldn\'t be loaded.",
+    "lists_empty":"Your lists are empty.",
+    "any_questions":"If you have any questions on how to implement the web part, please check the user guide.",
+    "lists":"Lists:",
+    "mixed_content":"Mixed content error",
+    "loaded_over_https":"This site was loaded over HTTPS but the provided url is being requested over HTTP",
+    "invalid_url":"Invalid URL",
+    "load_content_invalid_url":"This news article is trying to load content using an invalid url",
+    "wp_container":"Due to the size of this web part container, only ",
+    "wp_container2":"columns fit in this web part",
+    "wp_container3":"In bigger windows or page sections, the web part may display up to ",
+    "wp_container4":" columns.",
+    "no_entries":"No entries to display",
+    "show_details":"Show details...",
+    "hide_details":"Hide details...",
+    "wp_properties":"Web Part Properties",
+    "add_news":"Add News",
+    "page_checked_in":"Page is checked in! Check Out the page and save the web part for these changes to take effect.",
+    "new_item_saved":"With this option enabled, the web part will not update until the properties are saved or you add a new item using the web part\'s forms.",
+    "limited_space":"Browsers have limited space available for local storage. Other web parts or scripts resorting to LocalStorage may lose functionality when it becomes full.",
+    "save_create_another":"Save and create another",
+    "succesfuly_created":"News successfuly created",
+    "cant_save_properties":"Can\'t save properties before saving or deleting this field.",
+    "wp_properties_saved":"Web part properties saved successfully",
+    "something_went_wrong":"Oops, something went wrong:",
+    "news_successfuly":"News successfuly updated.",
+    "are_you_sure":"Are you sure you want to erase this news?",
+    "news_successfuly_deleted":"News successfully deleted",
+    "failed_delete_news":"Failed to delete news:",
+    "failed_retrieve_news":"Failed to retrieve news data:",
+    "couldnt_load_list_corrupted":"Couldn\'t load lists from site. Make sure there are no corrupted lists and apps on the site.",
+    "couldnt_load_view_fields":"Couldn\'t load view fields. Try loading the view again.",
+
+    "saving":"saving...",
+    "save_changes":"Save changes",
+    "save":"Save",
+    "save_create":"Save and create another",
+    "save_adding":"Adding...",
+  }
+};
