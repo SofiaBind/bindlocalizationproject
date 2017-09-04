85dcd11f2a261c14183eff4d38ccce3151996bcb
SofiaBind <31539042+SofiaBind@users.noreply.github.com>
2017-09-04 14:21:09 +0100

    Add files via upload

---
+++ BT-Web Parts/BT-Slider/slider en-us.js
@@ -0,0 +1,273 @@
+window.BT = window.BT || {};
+window.BT.v2sliderWP  = window.BT.v2sliderWP || {};
+window.BT.v2sliderWP.lang = window.BT.v2sliderWP.lang || {};
+window.BT.v2sliderWP.lang['en-us'] = {
+  "webpart_toolbar": {
+    "webpart_name"			: "Slider",
+    "new_entry"				: "Create new Slide",
+    "properties"			: "Web Part Properties",
+    "manage"				: "Manage Slider",
+    "list"					: "Slider List",
+    "version"				: "Version",
+    "page_not_checkout"		: "Page not checked out",
+    "awaiting_approval"		: "This web part has Slides waiting for approval.",
+    "preview_mode"			: "Preview mode"
+  },
+  "webpart_new_item": {
+    "modal_heading"				: "Create New Slide",
+    "edit_heading"				: "Edit Slide",
+
+    "checkin_warning_strong"	: "Page is checked in!",
+    "checkin_warning"			:  "Check Out the page and save the web part for these changes to take effect.",
+
+    "slide_settings_heading"	: "Slide Settings",
+    "slide_order_label"			: "Slide Order",
+
+    "caption_settings_heading"			: "Caption Settings",
+    "slide_background_label"			: "Slide Background",
+    "slide_background_option_color"		: "Color",
+    "slide_background_option_image"		: "Image",
+    "bg_img_url"						: "Background image URL",
+    "youtube_vimeo_sp"					: "YouTube, Vimeo or SharePoint Video Portal",
+    "slide_background_option_video"		: "Video",
+    "slide_background_placeholder"		: "e.g. #000000",
+    "slide_background_transparency"		: "Transparency",
+
+    "caption_text_label"							: "Caption Text",
+    "caption_text_placeholder"						: "Optional. Text to be displayed when marker is clicked on",
+    "caption_text_hyperlink_type_text"				: "Text Link",
+    "caption_text_hyperlink_type_slider"			: "Slider Link",
+    "caption_text_hyperlink_type_tiny_button"		: "Tiny Button",
+    "caption_text_hyperlink_type_small_button"		: "Small Button",
+    "caption_text_hyperlink_type_medium_button"		: "Medium Button",
+    "caption_text_hyperlink_type_large_button"		: "Large Button",
+    "caption_text_hyperlink_address"				: "Address",
+    "caption_text_hyperlink_display_text"			: "Display text",
+    "caption_text_hyperlink_bg_color"				: "e.g. #000000",
+    "caption_text_hyperlink_text_color"				: "Text color",
+    "caption_text_hyperlink_new_window"				: "Open in new window",
+    "caption_text_hyperlink_btn_apply"				: "Ok",
+
+    "caption_text_image_alt_placeholder"			: "Alt text",
+    "caption_text_image_path_placeholder"			: "Image path",
+    "caption_text_image_btn_apply"					: "Ok",
+
+    "caption_text_video_path_placeholder"			: "Video path",
+    "caption_text_video_btn_apply"					: "Ok",
+    "caption_text_info"								: "We support Markdown. Check syntax rules ",
+    "caption_text_info_here"						: "here",
+    "caption_bg_color"								: "Caption Background Color",
+    "caption_background_color_label"				: "e.g. #000000",
+    "caption_background_color_transparency"			: "Transparency",
+    "caption_background_set_transparency"			:"Set the color transparency",
+
+    "caption_width_label"							: "Caption Width",
+    "caption_width_placeholder"						: "eg: 800",
+
+    "caption_position_label"						: "Caption Position",
+    "caption_position_option_top"					: "Top",
+    "caption_position_option_center"				: "Center",
+    "caption_position_option_bottom"				: "Bottom",
+    "caption_position_option_left"					: "Left",
+    "caption_position_option_middle"				: "Center",
+    "caption_position_option_right"					: "Right",
+    "caption_position_error"						: "Please select 2 alignment values.",
+
+    "publishing_settings_heading"					: "Publishing Settings",
+    "publish_dates_label"							: "Publish Dates",
+    "publish_dates_start_placeholder"				: "Start Date",
+    "publish_dates_end_placeholder"					: "End Date",
+
+
+    "btn_publish"									: "Publish",
+    "btn_submit_review"								: "Submit for Review",
+    "btn_cancel"									: "Cancel",
+    "btn_preview"									: "Preview",
+    "btn_publish"									: "Publish",
+    "btn_waiting"									: "Waiting for Approval",
+    "btn_save_draft"								: "Save as Draft",
+    "btn_draft"										: "Draft",
+    "btn_save_review"								: "Submit for Review",
+    "btn_save_review_tooltip"						: "Saved for review and will be published by a reviewer.",
+    "btn_publish_tooltip"							: "Publish the slide. Will appear on the slider to all users.",
+    "btn_saving"									: "Saving...",
+    "btn_continue_editing"							: "Continue Editing"
+    
+  },
+
+  "webpart_properties": {
+    "modal_heading"									: "Web Part Properties",
+
+    "slider_options_heading"						: "Slider Options",
+    "settings_source_label"							: "Settings Source",
+    "settings_source_nothing"						: "Nothing Selected",
+    "settings_source_copy"							: "Copy",
+    "settings_source_copy_success"					: "Properties copied successfully.",
+
+    "slider_type_label"								: "List Options",
+    "slider_type_options_default"					: "Default Slider",
+    "slider_type_options_smart_slider"				: "Smart Slider",
+    "slider_lists_label"							: "Slider lists",
+    "slider_list_placeholder"						: "SharePoint site or list URL",
+    "slider_list_select_placeholder"				: "Select List",
+    "slider_list_select_view"						: "Select a View",
+    "filtering_options_label"						: "Filtering Options",
+    "filtering_options_nofilter"					: "No Filters",
+    "filtering_options_caml"						: "CAML Query",
+    "template_list_label"							: "Template List",
+    "source_list_label"								: "Source List",
+    "add_slider_btn"								: "Add Slider",
+
+    "item_sorting_heading"							: "Item Sorting Options",
+    "ascending"										: "Ascending",
+    "descending"									: "Descending",
+    "order_by_label"								: "Order By",
+    "add_order_by"									: "Add Order By",
+
+
+    "slider_styles_heading"							: "Slider Styles",
+    "slider_template_label"							: "Slider Template",
+    "slider_template_option_carousel"				: "Carousel",
+    "slider_template_option_small_thumbnails"		: "Small Thumbnails",
+    "slider_template_option_large_thumbnails"		: "Large Thumbnails",
+    "slider_template_option_3col_news"				: "3 Column News",
+    "slider_template_option_4col_news"				: "4 Column News",
+    "slider_template_option_3col_port"				: "3 Column Portfolio",
+    "slider_template_option_4col_port"				: "4 Column Portfolio",
+
+    "slider_indicators_label"						: "Slider Indicators",
+    "slider_indicators_option_none"					: "None",
+    "slider_indicators_option_bullets"				: "Bullets",
+    "slider_indicators_option_bumbers"				: "Numbers",
+
+    "slider_arrows_label"							: "Slider Arrows",
+    "slider_arrows_option_hide"						: "Hide",
+    "slider_arrows_option_show"						: "Show",
+
+    "slider_interval_label"							: "Slider Interval",
+    "slider_interval_seconds"						: "seconds",
+    "image_background_label"						: "Image Background",
+    "image_background_option_stretch"				: "Stretch",
+    "image_background_option_cover"					: "Cover",
+    "slider_height_label"							: "Slider Height",
+
+    "advanced_options_heading"						: "Advanced Options",
+    "move_to_slider_checkbox"						: "Move to slider zone",
+    "inherits_styles_master"						: "Inherit styles from master",
+    "retrieve_rich_content"							: "Retrieve rich content from source list",
+    "retrieve_rich_content_tip"						: "Support for this feature is not provided. Disables markdown. Recomended for experienced users only.",
+    "cache_webpart"									: "Cache web part for faster loading",
+
+    "caching_label"									: "Caching",
+    "caching_option_nocaching"						: "No Caching (Updates Every Page Load)",
+    "caching_option_page"							: "Page Caching (updates when web part is saved)",
+    "caching_option_localstorage"					: "Local Storage Caching (updates every 30 minutes)",
+    "caching_add_new_warning"						: "With this option enabled, the web part will not update until the properties are saved or you add a new item using the web part's forms.",
+    "caching_impact_info"							: "When the Smart Slider option is enabled, this option has little impact in performance.",
+    "caching_limited_space_warning"					: "Browsers have limited space available for local storage. Other web parts or scripts resorting to LocalStorage may lose functionality when it becomes full.",
+    "language_label"								: "Language",
+    "some_texts_warning"							: "Some texts may not be updated until the page gets refreshed.",
+
+    "web_part_appearance_heading"					: "Chrome Appearance",
+    "chrome_type_label"								: "Chrome Type",
+    "chrome_type_option_default"					: "Default",
+    "chrome_type_option_none"						: "None",
+    "chrome_type_option_title_border"				: "Title and Border",
+    "chrome_type_option_title"						: "Title Only",
+    "chrome_type_option_border"						: "Border Only",
+    "webpart_title_label"							: "BindTuning Quick Links",
+    "webpart_title_default_text"					: "BindTuning Calendar",
+    "webpart_url_label"								: "Web Part URL",
+    "webpart_tooltip_label"							: "Web Part Tooltip",
+    "webpart_tooltip_default_text"					: "Display content and make data interactive at the same time, creating sliding interfaces. Great for news and events rotators or to create image galleries.",
+    "rtl_label"										: "Right to Left",
+    "rtl_checkbox_label"							: "Active",
+
+
+    "publishing_workflow_heading"					: "Publishing Workflow",
+    "publishing_workflow_checkbox"					: "Enable Publishing Workflow",
+    "review_permissions"							: "Content Reviewers",
+    "review_permissions_placeholder"				: "Enter names or email addresses",
+
+    "webpart_feedback"								: "Web Part Feedback",
+
+    "webpart_errors_label"							: "Web Part errors",
+    "webpart_errors_option_edit"					: "Show In Edit Mode Only",
+    "webpart_errors_option_always"					: "Always Show",
+    "webpart_errors_option_never"					: "Never Show",
+    "loading_experience_label"						: "Loading Experience",
+    "loading_experience_option_none"				: "No Animations",
+    "loading_experience_option_animated"			: "Animated Bullets",
+    "loading_experience_option_lazy"				: "Lazy Loading",
+
+    "btn_copy"										: "Copy Properties",
+    "btn_cancel"									: "Cancel",
+    "btn_save"										: "Save"
+  },
+
+  "webpart_list": {
+    "modal_heading"				: "Slider List",
+    
+    "load_list"					: "Load Lists",
+    "order"						: "Order",
+    "active"					: "Active",
+    "background"				: "Background",
+    "caption"					: "Caption",
+    "status"					: "Status",
+    "list"						: "List",
+    "publish_start"				: "Publish Start",
+    "publish_end"				: "Publish End",
+    "delete_slider"				: "Are you sure you want to remove this slide?",
+    "delete_success"			: "Slide successfully deleted",
+    "no_entries"				: "No entries to display",    
+    "next_label"				: "Next",
+    "previous_label"			: "Previous",
+
+    "btn_publish"				: "Publish",
+    "btn_save_draft"			: "Save as Draft",
+    "btn_submit_review"			: "Submit for Review",
+
+
+
+    "tooltip_edit"				: "Edit",
+    "tooltip_view"				: "View",
+    "tooltip_delete"			: "Delete"
+  },
+
+  "webpart_messages":{
+    "wp_not_configured"				: "This Web Part is not fully configured.",
+	"cant_save"						: "Can\'t save properties before saving or deleting this field.",
+    "connect_to_list"				: "Open the Web Part properties and connect it to a list.",
+    "not_provided"					: "A source list was not provided",
+    "connect_smart_slider"			: "Smart sliders retrieve information from your SharePoint lists. Connect it to one of them to see their content displayed here",
+    "localstorage_full"				: "BT WebParts: Localstorage is full and the web part was not succesfully cached.",
+    "couldnt_load_lists"			: "It seems we couldn't load the provided lists. Please make sure you typed the correct URLs.",
+    "check_user_guide"				: "If you have any questions on how to implement the web part, please check the user guide.",
+    "list_label"					: "List(s): ",
+	"provided_lists_url"			: "Provided list Urls:",
+    "some_lists_couldnt_load"		: "Some lists couldn\'t be loaded.",
+    "list_empty"					: "Your list is empty. Please add data to your list.",
+    "wp_successfully_saved"			: "Web part properties successfully saved",
+    "something_wrong"				: "Oops, something went wrong:",
+    "slide_saved"					: "Slide successfuly saved",
+    "no_permission"					: "Oops, you don't have permission to make this change.",
+    "slide_updated" 				: "Slide was successfuly updated.",
+    "sure_want_remove"				: "Are you sure you want to remove this slide?",
+    "successfuly_deleted"			: "Slide was successfully deleted",
+    "failed_delete"					: "Failed to delete the slide:",
+    "failed_retrieve_data"			: "Failed to retrieve slide data:",
+    "sure_publish_slide"			: "Are you sure you want to publish this slide?",
+    "slide_successfully_published"	: "Slide was successfully published",
+    "failed_publish"				: "Failed to publish the slide:",
+    "list_not_loaded"				: "Something went wrong. Selected list could not be loaded.",
+    "couldnt_load_lists_corrupted"	: "Couldn\'t load lists from site. Make sure there are no corrupted lists and apps on the site.",
+    "site_not_found"				: "Site not found.",
+    "all_fields"					: "All Fields",
+    "filtering_options"				: "Filtering Options:",
+    "no_publish_permission"			: "Don't have permission to publish."
+  },
+
+  "custom_form":{
+    "no_filters"					: "No Filters"
+  }
+};
