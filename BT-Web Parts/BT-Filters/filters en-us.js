175d7464431c40da1e70b93f9e698e0c2af0c517
SofiaBind <31539042+SofiaBind@users.noreply.github.com>
2017-09-04 14:18:21 +0100

    Add files via upload

---
+++ BT-Web Parts/BT-Filters/filters en-us.js
@@ -0,0 +1,230 @@
+window.BT = window.BT || {};
+window.BT.v2filtersWP  = window.BT.v2filtersWP || {};
+window.BT.v2filtersWP.lang = window.BT.v2filtersWP.lang || {};
+window.BT.v2filtersWP.lang['en-us'] = {
+  "webpart_toolbar": {
+    "webpart_name": "Filters",
+    "description": "Description",
+    "title": "Title",
+    "all": "all",
+    "webpart_name": "Filters",
+    "new_entry": "Create new Template",
+    "properties": "Webpart Properties",
+    "manage": "Manage Templates",
+    "more": "More Options",
+    "items_list": "Items List...",
+    "templates_list": "Templates List...",
+    "version": "Version",
+	"preview_mode" : "Preview mode"
+  },
+  "webpart_new_item": {
+    "modal_heading": "Add New Template",
+
+    "template_settings_heading": "Template Settings",
+    "template_name_label": "Template Name",
+    "template_order_label": "Template Order",
+    "template_status_label": "Template Status",
+    "template_status_checkbox": "Active",
+
+    "link_setup_heading": "Setup a Link",
+    "clickable_link_label": "Clickable Link",
+    "clickable_link_placeholder": "Type the url to a page",
+    "clickable_link_checkbox": "Open link in a new window",
+
+    "front_tile_setting_heading": "Front Tile Settings",
+    "back_tile_setting_heading": "Back Tile Settings",
+    "tile_background_label": "Tile Background",
+    "tile_background_option_color": "Color",
+    "tile_background_option_image": "Image",
+    "tile_background_option_placeholder": "e.g. #000000",
+    "tile_background_option_bg_placeholder": "Background image URL",
+    "tile_background_option_transparency_placeholder": "Transparency",
+    "tile_background_option_transparency_set_placeholder": "Set the color transparency",
+    "tile_background_tip": "Choose between a background color or a background image",
+    "clickable_link_checkbox": "Open link in a new window",
+
+    "back_tile_setting_heading": "Back Tile Settings",
+
+    "caption_position_label": "Caption Position",
+    "caption_position_top": "Top",
+    "caption_position_center": "Center",
+    "caption_position_bottom": "Bottom",
+
+    "content_label": "Content",
+    "content_tip": "We support Markdown. Check syntax rules ",
+    "content_tip_link": "here",
+
+    "caption_bg": "Caption Background",
+    "caption_bg_placeholder": "e.g. #000000",
+    "caption_bg_transparency_placeholder": "Transparency",
+
+    "flip_settings_heading": "Flip Settings",
+    "flip_animation_label": "Flip Animation",
+    "flip_animation_option_none": "None",
+    "flip_animation_option_flip": "Flip",
+    "flip_animation_option_slideup": "Slide up",
+    "flip_animation_option_slidedown": "Slide down",
+    "flip_auto_label": "Auto Flip",
+    "flip_auto_placeholder": "Interval",
+    "flip_auto_seconds": "Seconds",
+
+    "btn_cancel": "Cancel",
+    "btn_preview": "Preview",
+    "btn_save": "Save",
+    "btn_save_changes": "Save Changes",
+    "btn_save_create": "Save and Create Another"
+
+  },
+
+  "webpart_properties": {
+    "modal_heading": "Web Part Properties",
+
+    "options_heading": "Filters Options",
+    "settings_source_label": "Settings Source",
+    "settings_source_nothing": "Nothing Selected",
+    "settings_source_copy": "Copy",
+    "settings_source_copy_success": "Properties copied successfully.",
+
+    "template_options_heading": "Template Options",
+    "template_list_url_label": "Templates List Url",
+    "template_list_url_placeholder": "e.g. https://contoso.sharepoint.com/Lists/Filters/AllItems.aspx",
+
+    "data_source_url_label": "Data Source Url",
+    "data_source_url_placeholder": "e.g. https://contoso.sharepoint.com/Lists/Filters/AllItems.aspx",
+
+    "advanced_options_heading": "Advanced Settings",
+    "caching_label": "Caching",
+    "caching_option_nocaching": "No Caching (Updates Every Page Load)",
+    "caching_option_localstorage": "Local Storage Caching (updates every 30 minutes)",
+    "caching_user_targetting_warning": "Templates with user targetting will not be displayed using this option.",
+    "caching_limited_space_warning": "Browsers have limited space available for local storage. Other web parts or scripts resorting to LocalStorage may lose functionality when it becomes full.",
+    "language_label": "Language",
+    "update_text_refresh":"Some texts may not be updated until the page gets refreshed.",
+
+    "wp_messages": "Web Part Messages",
+
+    "source_query_label": "Source Query",
+    "source_query_placeholder": "<view><query></query></view>",
+    "source_query_tip": "This field uses Caml Query. To create your query, get U2U Caml Query Builder ",
+    "source_query_tip_link": "here",
+
+	"filters_options_heading": "Filtering Options",
+    "filters_label": "Filters",
+    "filters_placeholder": "e.g. Editor, Document type, Sector",
+	"filters_layout_label": "Filters layout",
+	"filters_layout_option_ddown": "Dropdowns",
+	"filters_layout_option_btn": "Buttons",
+    "open_filters_label": "Open filters on",
+    "open_filters_option_hover": "Hover",
+    "open_filters_option_click": "Click",
+
+    "tile_options_heading": "Tile Options",
+    "grid_size_label": "Grid Size",
+    "grid_size_option_extra_tiny": "Extra Tiny",
+    "grid_size_option_tiny": "Tiny",
+    "grid_size_option_small": "Small",
+    "grid_size_option_medium": "Medium",
+    "grid_size_option_large": "Large",
+
+    "tiles_spacing_label": "Tiles Spacing",
+    "tiles_spacing_option_0": "Zero",
+    "tiles_spacing_option_5": "Five",
+    "tiles_spacing_option_10": "Ten",
+    "tiles_spacing_option_20": "Twenty",
+
+    "locked_grid_label": "Locked Grid",
+    "locked_grid_checkbox": "Locked",
+    "locked_grid_tip": "Prevent tiles from making small size adjustments to fill all available horizontal space.",
+
+	"web_part_appearance_heading" : "Chrome Appearance",
+    "chrome_type_label": "Chrome Type",
+    "chrome_type_option_default": "Default",
+    "chrome_type_option_none": "None",
+    "chrome_type_option_title_border": "Title and Border",
+    "chrome_type_option_title": "Title Only",
+    "chrome_type_option_border": "Border Only",
+    "webpart_title_label": "Web Part Title",
+    "webpart_title_default_text": "BindTuning Calendar",
+    "webpart_url_label": "Web Part URL",
+    "webpart_tooltip_label": "Web Part Tooltip",
+    "webpart_tooltip_default_text": "Display your SharePoint lists data in interesting and intuitive ways powered by automatic metadata-based filtering. Customize to your needs and connect to any type of content.",
+    "rtl_label": "Right to Left",
+    "rtl_checkbox_label": "Active",
+
+    "calendar_webpart_errors_label": "Web Part errors",
+    "calendar_webpart_errors_option_edit": "Show In Edit Mode Only",
+    "calendar_webpart_errors_option_always": "Always Show",
+    "calendar_webpart_errors_option_never": "Never Show",
+
+    "btn_copy": "Copy Properties",
+    "btn_cancel": "Cancel",
+    "btn_save": "Save"
+  },
+
+  "template_list": {
+    "modal_heading": "Template List",
+
+    "title": "Title",
+    "order": "Order",
+    "active": "Active",
+    "front_background": "Front Background",
+    "back_background": "Back Background",
+    "duplicate_success": "Template copy was successfully created",
+    "delete_alert": "Are you sure you want to delete this template?",
+    "delete_success": "Template was successfully deleted",
+    "no_entries": "No entries to display"
+
+  },
+
+  "webpart_document_modal": {
+    "download_btn": "Download",
+    "share_btn": "Share",
+    "close_btn": "Close"
+  },
+
+  "webpart_messages":{
+    "failed_to_save":"Failed do save properties: ",
+    "failed_to_get":"Failed to get properties from webpart: ",
+    "cannot_preview" :'Cannot preview template. Web part property "Data Source URL" is not defined',
+    "continue_editing" : "Continue Editing",
+    "no_results":"Your source list returned no results. Make sure you have content on the list and are using the right query.",
+    "any_questions":"If you have any questions on how to implement the filters web part, please check the user guide.",
+    "couldnt_connect":"Couldn\'t connect to source list",
+    "saved_success" :"Web part properties saved successfully",
+
+    "something_wrong": "Oops, something went wrong:",
+    "due_to_update1":"Due to a web part update, this form is currently not working.",
+    "due_to_update2":"To fix this issue:",
+    "due_to_update3":"1. Open the SharePoint web part properties",
+    "due_to_update4":"2. Once loaded, press OK and wait for the page to refresh",
+    "due_to_update5":"3. Save the page.",
+
+    "failed_template":"Failed to retrieve template:",
+    "success_update":'Template was successfuly updated.',
+    "failed_copy":"Failed to copy template:",
+    "failed_delete":"Failed to delete template:",
+
+    "properties_copied":"Properties copied successfully.",
+
+    "saving":"Saving...",
+    "adding":"Adding...",
+    "successfuly_added":"Template succesfuly added",
+	
+	"wp_not_configured"  : "This Web Part is not fully configured.",
+	"connect_to_list"    : "Open the Web Part properties and connect it to a list.",
+	"connect_to_data_list" : "Please fill out the 'Data Source URL' field in the web part properties.",
+	"not_provided"		 : "A source list was not provided",
+	"connect_to_template": "It seems we couldn't load the templates list. Please make sure you typed the correct URL.",
+    "check_user_guide"	 : "If you have any questions on how to implement the web part, please check the user guide.",
+	"debug_message_label": "Debug message:",
+	"template_list_empty": "Your templates list is empty. Please create a template for how your data should be displayed",
+	"data_list_empty"    : "Your source list returned no results. Make sure you have content on the list and are using the right query.",
+	"couldnt_load_list"  : "It seems we couldn't load the provided list. Please make sure you typed the correct URL.",
+	"provided_lists_url" : "Provided list Urls:"
+  },
+
+  "webpart_custom":{
+    "edit_template": "Edit Template",
+    "new_template":"Add new Template"
+  }
+};
