0e52e68e2dd4073cd31d67bc3f95184024b239e7
SofiaBind <31539042+SofiaBind@users.noreply.github.com>
2017-09-04 14:17:46 +0100

    Add files via upload

---
+++ BT-Web Parts/BT-Calendar/calendar en-us.js
@@ -0,0 +1,189 @@
+window.BT = window.BT || {};
+window.BT.v2calendarWP  = window.BT.v2calendarWP || {};
+window.BT.v2calendarWP.lang = window.BT.v2calendarWP.lang || {};
+window.BT.v2calendarWP.lang['en-us'] = {
+  "regional_settings":{
+    "moment" : "en"
+  },
+  "webpart_content": {
+    "no_events_today": "No events happening today",
+    "no_events_future": "No upcoming events",
+    "no_events_past": "No recent past events",
+    "event_allday":  "All day"
+  },
+  "webpart_toolbar": {
+    "webpart_name" : "Calendar",
+    "new_entry": "Create new Event",
+    "properties": "Webpart Properties",
+    "view_calendar": "View Calendar",
+    "version": "Version",
+    "page_not_checkout": "Page not checked out"
+  },
+  "webpart_properties": {
+    "modal_heading": "Web Part Properties",
+
+    "calendar_options_heading": "Calendar Options",
+    "settings_source_label": "Settings Source",
+    "settings_source_nothing": "Nothing Selected",
+    "settings_source_copy": "Copy",
+    "settings_source_copy_success": "Properties copied successfully.",
+
+    "content_heading": "Event Source",
+
+    "sharepoint_calendars": "SharePoint Calendars",
+    "path_placeholder": "e.g. /sites/Home/SitePages/default.aspx",
+    "select_calendar": "Select Calendar",
+    "overlays_label": "Calendar Overlays:",
+    "overlays_warning": "Calendar has no overlays",
+    "search_results": "Search Results",
+    "recurrent_warning": "Recurrent events are not supported by search",
+    "search_option_all": "Search all of SharePoint",
+    "search_option_site": "Search a site",
+    "search_option_site_collection": "Search a site collection",
+    "search_site_url_label": "Site URL",
+    "add_btn": "Add Calendar",
+
+
+    "filter_heading": "Calendar Filters",
+
+    "layout_label": "Calendar Layout",
+    "layout_option1":"Layout 1",
+    "layout_option2":"Layout 2",
+    "layout_option3":"Layout 3",
+    "layout_option4":"Layout 4",
+    "layout_option5":"Layout 5",
+
+    "filter_categories_label": "Filter by categories (default)",
+    "filter_categories_show_all": "Show All",
+    "filter_categories_meeting": "Meeting",
+    "filter_categories_work_hours": "Work Hours",
+    "filter_categories_business": "Business",
+    "filter_categories_get_together": "Get Together",
+    "filter_categories_gifts": "Gifts",
+    "filter_categories_birthday": "Birthday",
+    "filter_categories_anniversary": "Anniversary",
+
+    "filter_categories_custom_label": "Filter by categories (custom)",
+    "filter_categories_custom_placeholder": "e.g. Category1; Category2",
+
+    "filter_location_label": "Filter by location",
+    "filter_location_placeholder": "e.g. Location1; Location2",
+
+    "events_heading": "Calendar Events",
+
+    "time_period_label": "Time Period",
+    "time_period_option_past": "Past",
+    "time_period_option_future": "Future",
+    "time_period_option_today": "Today",
+
+    "time_range_label": "Time Range",
+    "time_range_option_3hours": "3 Hours",
+    "time_range_option_6hours": "6 Hours",
+    "time_range_option_12hours": "12 Hours",
+    "time_range_option_1day": "1 Day",
+    "time_range_option_3days": "3 Days",
+    "time_range_option_1week": "1 Week",
+    "time_range_option_2weeks": "2 Weeks",
+    "time_range_option_1month": "1 month",
+    "time_range_option_3months": "3 months",
+    "time_range_option_6months": "6 months",
+    "time_range_option_1year": "1 year",
+
+    "event_limit_label": "Event limit",
+
+    "highlights_events_user_late_label": "Highlight events when user is late",
+    "highlights_events_user_late_option_no": "No",
+    "highlights_events_user_late_option_1hour": "Event started and ends in less than 1 hour",
+    "highlights_events_user_late_option_12hours": "Event started and ends in less than 12 hours",
+
+    "highlights_upcoming_events_label": "Highlight upcoming events",
+    "highlights_upcoming_events_option_no": "No",
+    "highlights_upcoming_events_option_next_24hours": "Event starts in the next 24 hours",
+    "highlights_upcoming_events_option_next_48hours": "Event starts in the next 48 hours",
+
+    "recurrent_events_label": "Recurrent events",
+    "recurrent_events_checkbox_label": "Hide",
+    "all_day_events_label": "All day events",
+    "all_day_events_checkbox_label": "Hide",
+
+    "settings_heading": "Category Settings",
+
+    "color_coding_label": "Color coding",
+    "color_coding_checkbox_label": "Enabled",
+    "color_coding_settings_label": "Color coding settings",
+    "color_coding_settings_placeholder": "e.g. Category1[color];Category2[color];",
+    "custom_icons_label": "Custom icons",
+    "custom_icons_checkbox_label": "Enabled",
+    "custom_icon_settings_label": "Custom icons settings",
+    "custom_icon_settings_placeholder": "e.g. Category1[icon class]; Category2[icon class]; Category2,Category3[icon class]",
+    "custom_icon_settings_tip": "To use custom category icons, check Font Awesome examples",
+    "custom_icon_settings_tip_link": "here",
+
+    "advanced_options_heading": "Advanced Options",
+    "caching_label": "Caching",
+    "caching_option_nocaching": "No Caching (Updates Every Page Load)",
+    "caching_option_localstorage": "Local Storage Caching (updates every 30 minutes)",
+    "caching_user_targetting_warning": "Links with user targetting will not be displayed using this option.",
+    "caching_limited_space_warning": "Browsers have limited space available for local storage. Other web parts or scripts resorting to LocalStorage may lose functionality when it becomes full.",
+    "language_label": "Language",
+    "some_texts_warning": "Some texts may not be updated until the page gets refreshed.",
+    "webpart_appearance": "Web Part Appearance",
+
+    "chrome_type_label": "Chrome Type",
+    "chrome_type_option_default": "Default",
+    "chrome_type_option_none": "None",
+    "chrome_type_option_title_border": "Title and Border",
+    "chrome_type_option_title": "Title Only",
+    "chrome_type_option_border": "Border Only",
+    "webpart_title_label": "Web Part Title",
+    "webpart_title_default_text": "BindTuning Calendar",
+    "webpart_url_label": "Web Part URL",
+    "webpart_tooltip_label": "Web Part Tooltip",
+    "webpart_tooltip_default_text": "Supercharge your events with a milestone calendar, filter events to be displayed, and color-code your events, making planning easy and intuitive.",
+    "rtl_label": "Right to Left",
+    "rtl_checkbox_label": "Active",
+
+    "webpart_messages": "Web Part Messages",
+
+    "webpart_errors_label": "Web Part errors",
+    "webpart_errors_option_edit": "Show In Edit Mode Only",
+    "webpart_errors_option_always": "Always Show",
+    "webpart_errors_option_never": "Never Show",
+
+    "results_message_label": "No results message",
+    "webpart_message_option_edit": "Show In Edit Mode Only",
+    "webpart_message_option_always": "Always Show",
+    "webpart_message_option_never": "Never Show",
+
+    "btn_copy": "Copy Properties",
+    "btn_cancel": "Cancel",
+    "btn_save": "Save"
+  },
+
+  "webpart_messages": {
+    "no_events": "No events have been found.",
+    "cant_save" : "Can\'t save properties before saving or deleting this field.",
+    "something_went_wrong" : "Something went wrong. Selected calendar could not be loaded.",
+    "couldnt_load_calendars" : "Couldn\'t load calendars from site. Make sure there are no corrupted lists and apps on the site.",
+    "save_success" : "Web part properties saved successfully",
+    "failed_to_save": "Failed to save properties: ",
+    "no_overlays" : "Calendar has no overlays",
+    "went_wrong" : "Oops, something went wrong:",
+    "site_not_found" : "Site not found",
+    "no_calendar" : "No Calendar",
+    "webpart_not_configured": "This Web Part is not fully configured.",
+    "list_not_connected": "Open the Web Part properties and connect it to a list.",
+    "localstorage_full": "Localstorage is full and the web part was not succesfully cached.",
+    "search_failed": "It seems we couldn't execute your query. Please make sure the search query syntax is correct.",
+    "search_failed_guide": "If you have any questions on how to use the search query, please check the user guide.",
+    "search_failed_provided": "Provided search query",
+    "lists_failed": "It seems we couldn't load the provided calendars. Please make sure you typed the correct URLs.",
+    "lists_failed_guide": "If you have any questions on how to implement the web part, please check the user guide.",
+    "lists_failed_provided": "Provided calendar Urls",
+    "lists_failed_partial": "'Some calendars couldn\'t be loaded.'"
+  },
+
+  "webpart_custom_form": {
+    "select_calendar": "Select Calendar"
+  }
+}
